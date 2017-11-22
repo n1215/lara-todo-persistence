2eed2b0a0573fe5fc3245483478067fda6d7d2a6
n1215 <nakae@nextat.co.jp>
2017-11-22 18:50:56 +0900

    パターン5 POPP+PDOのサンプルを追加

---
+++ app/Impls/POPOAndPDO/TodoItemRepository.php
@@ -0,0 +1,135 @@
+<?php
+declare(strict_types=1);
+
+namespace N1215\LaraTodo\Impls\POPOAndPDO;
+
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Collection;
+use N1215\LaraTodo\Common\TodoItemId;
+use N1215\LaraTodo\Common\TodoItemInterface;
+use N1215\LaraTodo\Common\TodoItemRepositoryInterface;
+use N1215\LaraTodo\Exceptions\PersistenceException;
+
+/**
+ * POPOのエンティティのためのQuery Builderによるレポジトリ
+ * Class TodoItemRepository
+ * @package N1215\LaraTodo\Impls\POPOAndPDO
+ */
+class TodoItemRepository implements TodoItemRepositoryInterface
+{
+    const SQL_SELECT = "select * from todo_items where id = :id limit 1";
+
+    const SQL_SELECT_ALL = "select * from todo_items";
+
+    const SQL_INSERT = "insert into todo_items (title, completed_at) VALUES (:title, :completed_at)";
+
+    const SQL_UPDATE = "update todo_items set title = :title, completed_at = :completed_at where id = :id";
+
+    /**
+     * @var TodoItemFactory
+     */
+    private $factory;
+
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(TodoItemFactory $factory, \PDO $pdo)
+    {
+        $this->factory = $factory;
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function find(TodoItemId $id): ?TodoItemInterface
+    {
+        $rawId = $id->getValue();
+        $statement = $this->pdo->prepare(self::SQL_SELECT);
+        $statement->bindParam(':id', $rawId);
+        $statement->execute();
+        $record = $statement->fetch();
+        $statement->closeCursor();
+
+        if (is_null($record)) {
+            return null;
+        }
+
+        return $this->factory->fromArray($record);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function list(): Collection
+    {
+        $statement = $this->pdo->prepare(self::SQL_SELECT_ALL);
+        $statement->execute();
+        $records = $statement->fetchAll();
+
+        return collect($records)
+            ->map(function (array $record) {
+                return $this->factory->fromArray($record);
+            });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function persist(TodoItemInterface $todoItem): TodoItemInterface
+    {
+        if (!$todoItem instanceof TodoItem) {
+            throw new \InvalidArgumentException('このリポジトリで永続化できるエンティティは' . TodoItem::class. 'のみです');
+        }
+
+        $rawId = $todoItem->getId()->getValue();
+        $completedAt = $todoItem->getCompletedAt()->getValue();
+        $rawCompletedAt = $completedAt ? $completedAt->format('Y-m-d H:i:s') : null;
+        $values = [
+            'title' => $todoItem->getTitle()->getValue(),
+            'completed_at' => $rawCompletedAt
+        ];
+
+        // 更新
+        if (!is_null($rawId)) {
+            try {
+                $statement = $this->pdo->prepare(self::SQL_UPDATE);
+                $statement->bindParam(':title', $values['title']);
+                $statement->bindParam(':completed_at', $values['completed_at']);
+                $statement->bindParam(':id', $rawId);
+                $statement->execute();
+            } catch(\Exception $e) {
+                logger()->error($e);
+                throw new PersistenceException('Todo項目の永続化に失敗しました。title=' . $todoItem->getTitle()->getValue(), 0, $e);
+            }
+
+            return $todoItem;
+        }
+
+        // 新規追加
+        try {
+            $statement = $this->pdo->prepare(self::SQL_INSERT);
+            $statement->bindParam(':title', $values['title']);
+            $statement->bindParam(':completed_at', $values['completed_at']);
+            $statement->execute();
+            $rawId = $this->pdo->lastInsertId();
+        } catch(\Exception $e) {
+            throw new PersistenceException('Todo項目の永続化に失敗しました。title=' . $todoItem->getTitle()->getValue(), 0, $e);
+        }
+
+        return $this->factory->fromArray(array_merge($values, ['id' => $rawId]));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function new(array $record): TodoItemInterface
+    {
+        return $this->factory->fromArray($record);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
         2 => Impls\EntityContainsEloquent\TodoItemRepository::class,
         3 => Impls\POPOAndEloquent\TodoItemRepository::class,
         4 => Impls\POPOAndQueryBuilder\TodoItemRepository::class,
+        5 => Impls\POPOAndPDO\TodoItemRepository::class,
     ];
 
     /**
@@ -27,6 +28,14 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // レポジトリの実装切り替え可 パターン 1〜4
-        $this->app->singleton(TodoItemRepositoryInterface::class, $this->repositoryClasses[1]);
+        $this->app->singleton(TodoItemRepositoryInterface::class, $this->repositoryClasses[5]);
+
+        // PDO用の設定
+        $this->app->singleton(Impls\POPOAndPDO\TodoItemRepository::class, function () {
+            return new Impls\POPOAndPDO\TodoItemRepository(
+                new Impls\POPOAndPDO\TodoItemFactory(),
+                new \PDO('sqlite:'. config('database.connections.sqlite.database'))
+            );
+        });
     }
 }
